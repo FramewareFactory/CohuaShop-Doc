a8c7f3bf221e96b7b678720acce193c17777c0bd
Diego <diego.estrada@fygdevelopers.com>
2021-09-27 13:55:05 -0500

    Primer commit

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,7 +2,7 @@ const { description } = require('../../package')
 
 module.exports = {
   dest: 'docs',
-  base: '/Cohuashop-doc/',
+  base: '/CohuaShop-Doc/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
